3ea76eb049ebb2c1bec3e58146315d55e6a0e33e
tolya-yanot <tolya-yanot>
2022-02-08 01:32:59 +0300

    update

--- nft/compile.sh
+++ nft/compile.sh
@@ -1,2 +1,11 @@
-func -SPA stdlib.fc nft.fc -o build/nft-code.fif
-func -SPA stdlib.fc nft-collection.fc -o build/nft-collection-code.fif
+rm build/nft-item-code.fif
+rm build/nft-collection-code.fif
+rm build/nft-marketplace-code.fif
+rm build/nft-sale-code.fif
+
+func -o build/nft-item-code.fif -SPA stdlib.fc nft-item.fc
+func -o build/nft-collection-code.fif -SPA stdlib.fc nft-collection.fc
+func -o build/nft-marketplace-code.fif -SPA stdlib.fc nft-marketplace.fc
+func -o build/nft-sale-code.fif -SPA stdlib.fc nft-sale.fc
+
+fift -s build/print-hex.fif
