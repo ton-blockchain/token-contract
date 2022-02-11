ac29cb375aaf578a71233ff9e089561bcf924971
emelyanenkok <emelyanenko.kirill@gmail.com>
2022-02-11 17:36:48 +0300

    Use op-codes from standard

--- nft/compile.sh
+++ nft/compile.sh
@@ -3,9 +3,9 @@ rm build/nft-collection-code.fif
 rm build/nft-marketplace-code.fif
 rm build/nft-sale-code.fif
 
-func -o build/nft-item-code.fif -SPA stdlib.fc nft-item.fc
-func -o build/nft-collection-code.fif -SPA stdlib.fc nft-collection.fc
-func -o build/nft-marketplace-code.fif -SPA stdlib.fc nft-marketplace.fc
-func -o build/nft-sale-code.fif -SPA stdlib.fc nft-sale.fc
+func -o build/nft-item-code.fif -SPA stdlib.fc op-codes.fc nft-item.fc
+func -o build/nft-collection-code.fif -SPA stdlib.fc op-codes.fc nft-collection.fc
+func -o build/nft-marketplace-code.fif -SPA stdlib.fc op-codes.fc nft-marketplace.fc
+func -o build/nft-sale-code.fif -SPA stdlib.fc op-codes.fc nft-sale.fc
 
 fift -s build/print-hex.fif
