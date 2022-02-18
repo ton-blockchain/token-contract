5ec5be4f335d9b9da289a741d70c3ad93947d735
tolya-yanot <tolya-yanot>
2022-02-18 22:29:24 +0300

    add nft-item-editable-DRAFT.fc

--- nft/compile.sh
+++ nft/compile.sh
@@ -1,9 +1,12 @@
 rm build/nft-item-code.fif
+rm build/nft-item-editable-code.fif
 rm build/nft-collection-code.fif
+rm build/nft-collection-editable-code.fif
 rm build/nft-marketplace-code.fif
 rm build/nft-sale-code.fif
 
 func -o build/nft-item-code.fif -SPA stdlib.fc op-codes.fc nft-item.fc
+func -o build/nft-item-editable-code.fif -SPA stdlib.fc op-codes.fc nft-item-editable-DRAFT.fc
 func -o build/nft-collection-code.fif -SPA stdlib.fc op-codes.fc nft-collection.fc
 func -o build/nft-collection-editable-code.fif -SPA stdlib.fc op-codes.fc nft-collection-editable.fc
 func -o build/nft-marketplace-code.fif -SPA stdlib.fc op-codes.fc nft-marketplace.fc
