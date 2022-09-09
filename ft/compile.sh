a6d6c78824bbe9472109e1d0a588c083e3d621fd
sasha1618 <sasha.k.1618@gmail.com>
2022-09-09 11:12:46 +0300

    feat: add addr validation and compilation process

--- ft/compile.sh
+++ ft/compile.sh
@@ -2,5 +2,7 @@ func -SPA -o ./build/jetton-wallet.fif ../stdlib.fc params.fc op-codes.fc jetton
 echo '"build/jetton-wallet.fif" include 2 boc+>B "build/jetton-wallet.boc" B>file' | fift -s
 func -SPA -o ./build/jetton-minter.fif ../stdlib.fc params.fc op-codes.fc jetton-utils.fc jetton-minter.fc
 func -SPA -o ./build/jetton-minter-ICO.fif ../stdlib.fc params.fc op-codes.fc jetton-utils.fc jetton-minter-ICO.fc
+func -SPA -o ./build/jetton-discovery.fif ../stdlib.fc params.fc op-codes.fc discovery-params.fc jetton-utils.fc jetton-discovery.fc
+func -SPA -o ./build/jetton-minter-discoverable.fif ../stdlib.fc params.fc op-codes.fc discovery-params.fc jetton-utils.fc jetton-minter-discoverable.fc
 
 fift -s build/print-hex.fif
